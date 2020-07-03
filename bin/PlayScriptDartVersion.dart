1499a73626edbdc339d555106f6efe2c9619ac37
connorlu <connorlu@tencent.com>
2020-07-03 22:26:20 +0800

    feat(all): init rpository

---
+++ bin/PlayScriptDartVersion.dart
@@ -0,0 +1,5 @@
+import 'package:PlayScriptDartVersion/PlayScriptDartVersion.dart' as PlayScriptDartVersion;
+
+void main(List<String> arguments) {
+  print('Hello world: ${PlayScriptDartVersion.calculate()}!');
+}
---
+++ test/PlayScriptDartVersion_test.dart
@@ -0,0 +1,8 @@
+import 'package:PlayScriptDartVersion/PlayScriptDartVersion.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
